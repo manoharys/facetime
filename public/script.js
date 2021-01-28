f43af9aab8cbf61c356e82457351dbb7604ee7ac
manohar <manumanohar1527@gmail.com>
2021-01-28 15:07:59 +0530

    rendering video stream

--- public/script.js
+++ public/script.js
@@ -1,1 +1,27 @@
-console.log(" js loaded");
+//DOM elements
+const video_grid = document.querySelector(".video-grid");
+
+
+//Creating video element dynamically
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+//Getting access to media input devices
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+})
+.then(stream=>{
+    addVideoStream(myVideo, stream);
+})
+
+//Adding video streams
+const addVideoStream = (video, stream)=>{
+   video.srcObject = stream;
+   video.addEventListener("loadedmetadata", ()=>{
+       video.play();
+       
+   })
+   video_grid.append(video);
+   
+}
