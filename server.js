6ab063eaa689b990a28bac94a62e280832ccee86
manohar <manumanohar1527@gmail.com>
2021-01-28 14:38:00 +0530

    setting up views

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const { v4: uuidV4 } = require("uuid");
 
-app.get('/', (req, res)=>{
-    res.send("hello world");
-})
+//Loading static data
+app.use(express.static("public"));
+
+//setting up views
+app.set("view engine", "ejs");
+
+//Generating random ID's
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+//Getting random generated Id's
+app.get("/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.roomId });
+});
 
 server.listen(process.env.PORT || 3000);
